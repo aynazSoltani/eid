2848add278817ccd3d2ed5aa74278ea3d7982a2a
unknown <aynazsln2005@gmail.com>
2025-04-21 16:44:26 +0330

    update  delete metod of Database class

--- src/db/Database.java
+++ src/db/Database.java
@@ -8,6 +8,8 @@ public class Database {
     private static ArrayList<Entity> entities = new ArrayList<>();
     private static int number = 1;
 
+    private Database() {};
+
    public static void add(Entity e){
        e.id = number++;
      entities.add(e);
@@ -21,17 +23,22 @@ public class Database {
        throw  new EntityNotFoundException(id);
     }
     public  static void delete(int id) throws EntityNotFoundException{
-       for(Entity e : entities){
-           if(e.id == id ){
-               entities.remove(e);
+       for(int i=0;i<entities.size();i++ ){
+           if(entities.get(i).id == id ){
+               entities.remove(i);
+               return;
            }
        }
        throw new EntityNotFoundException(id);
     }
     public static void update(Entity e) throws EntityNotFoundException{
-       for(Entity e : entities){
-           if(e.id == )
+       for(int i=0;i<entities.size();i++){
+           if(entities.get(i).id ==  e.id ){
+               entities.set(i,e);
+               return;
+           }
        }
+       throw new EntityNotFoundException(e.id);
     }
 }
 
