1d28c1c1b5393144d719f38e905d1b2827b48b59
unknown <aynazsln2005@gmail.com>
2025-05-10 12:23:51 +0330

    Add validator to Database.

--- src/db/Database.java
+++ src/db/Database.java
@@ -1,16 +1,31 @@
 package db;
 
 import db.exception.EntityNotFoundException;
+import db.exception.InvalidEntityException;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Database {
     private static ArrayList<Entity> entities = new ArrayList<>();
     private static int number = 1;
+    private static HashMap<Integer, Validator> validators = new HashMap<>();
 
     private Database() {};
 
-   public static void add(Entity e){
+    public static void registerValidator(int entityCode, Validator validator){
+        if (validators.containsKey(entityCode))
+            throw new IllegalStateException("Validator for this entity already exists.");
+
+        validators.put(entityCode, validator);
+    }
+
+
+    public static void add(Entity e)throws InvalidEntityException{
+       Validator validator = validators.get(e.getEntityCode());
+       if (validator != null) {
+           validator.validate(e);
+       }
        e.id = number++;
        Entity copy = e.copy();
      entities.add(copy);
@@ -32,8 +47,13 @@ public class Database {
        }
        throw new EntityNotFoundException(id);
     }
-    public static void update(Entity e) throws EntityNotFoundException{
+    public static void update(Entity e) throws EntityNotFoundException, InvalidEntityException{
        Entity copy = e.copy();
+        Validator validator = validators.get(e.getEntityCode());
+        if (validator != null) {
+            validator.validate(e);
+        }
+
        for(int i = 0;i<entities.size();i++){
            if(entities.get(i).id ==  copy.id ){
                entities.set(i,copy);
