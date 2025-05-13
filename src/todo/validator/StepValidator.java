cc35ed9d28648086f1a88d3537db0b9cbdc84245
unknown <aynazsln2005@gmail.com>
2025-05-13 13:58:05 +0330

    Add StepValidator.

--- src/todo/validator/StepValidator.java
+++ src/todo/validator/StepValidator.java
@@ -1,4 +1,27 @@
 package todo.validator;
 
-public class StepValidator {
+import db.Database;
+import db.Entity;
+import db.Validator;
+import db.exception.InvalidEntityException;
+import todo.entity.Step;
+
+public class StepValidator implements Validator {
+    @Override
+    public void validate(Entity entity) throws InvalidEntityException{
+        if (! (entity instanceof Step))
+            throw new IllegalArgumentException("Entity type must be Step.");
+
+        Step step = (Step) entity;
+        if (step.getTitle() == null || step.getTitle().isEmpty())
+            throw new InvalidEntityException("Title Cannot be null or empty.");
+
+        try {
+            Database.get(step.getTaskRef());
+        }
+        catch (Exception e){
+            throw new InvalidEntityException("Task with id " + step.getTaskRef() + "not found.");
+        }
+    }
+
 }
