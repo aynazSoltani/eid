f86fa2a67dfc652a072d0c4f64963bc67217308c
unknown <aynazsln2005@gmail.com>
2025-05-13 13:56:39 +0330

    Add TaskService class.

--- src/todo/service/TaskService.java
+++ src/todo/service/TaskService.java
@@ -1,4 +1,41 @@
 package todo.service;
 
+import db.Database;
+import db.exception.InvalidEntityException;
+import todo.entity.Task;
+
+
 public class TaskService {
+    public static void setAsCompleted(int taskId) {
+        try {
+            Task task = (Task) Database.get(taskId);
+            task.setStatus(Task.Status.Completed);
+            Database.update(task);
+        } catch (InvalidEntityException e) {
+            System.err.println("Error: Task with ID " + taskId + " not found.");
+        }
+    }
+
+
+    public static void setAsInProgress(int taskId) {
+        try {
+            Task task = (Task) Database.get(taskId);
+            task.setStatus(Task.Status.InProgress);
+            Database.update(task);
+        } catch (InvalidEntityException e) {
+            System.err.println("Error: Task with ID " + taskId + " not found.");
+
+        }
+    }
+
+    public static void setAsNotStarted(int taskId) {
+        try {
+            Task task = (Task) Database.get(taskId);
+            task.setStatus(Task.Status.NotStarted);
+            Database.update(task);
+        } catch (InvalidEntityException e) {
+            System.err.println("Error: Task with ID " + taskId + " not found.");
+
+        }
+    }
 }
