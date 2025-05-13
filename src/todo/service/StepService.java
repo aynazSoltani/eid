787acd5f091173c3d2ec35c843017fb2b438e415
unknown <aynazsln2005@gmail.com>
2025-05-13 13:55:55 +0330

    Add StepService.

--- src/todo/service/StepService.java
+++ src/todo/service/StepService.java
@@ -1,4 +1,34 @@
 package todo.service;
 
+import db.Database;
+import db.exception.InvalidEntityException;
+import todo.entity.Step;
+
 public class StepService {
+
+    public static void saveStep(int taskRef, String title) throws InvalidEntityException {
+        if (title == null || title.isEmpty())
+            throw new InvalidEntityException("Title Cannot  be null or empty.");
+
+        Step newStep = new Step(title, taskRef);
+        newStep.setStatus(Step.Status.NotStarted);
+        Database.add(newStep);
+    }
+
+    public static void setStepAsCompleted(int stepId) throws InvalidEntityException {
+        Step step = (Step) Database.get(stepId);
+        step.setStatus(Step.Status.Completed);
+        Database.update(step);
+    }
+
+    public static void setStepAsInProgress(int stepId) throws InvalidEntityException {
+        Step step = (Step) Database.get(stepId);
+        step.setStatus(Step.Status.NotStarted);
+        Database.update(step);
+    }
+
+    public static void deleteStep(int stepId) throws InvalidEntityException {
+        Step step = (Step) Database.get(stepId);
+        Database.delete(stepId);
+    }
 }
